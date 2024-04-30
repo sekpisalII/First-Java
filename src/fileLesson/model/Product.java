a00a3b83439bdadf4cb82fef1a12fe6c37acf505
pisalsek99 <pisalsek88@gmail.com>
2024-04-30 11:11:22 +0700

    extra code

--- src/fileLesson/model/Product.java
+++ src/fileLesson/model/Product.java
@@ -4,10 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 public class Product {
     private String uuid;
     private String ProductName;
